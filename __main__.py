3b5b3ba2c949467062fcbcd2a789660b90f0fa72
Phillip Logan <philo3194@gmail.com>
2022-12-04 04:40:35 +0000

    first commit

---
+++ __main__.py
@@ -0,0 +1,24 @@
+from urllib.parse import urlparse
+import os
+import psycopg2
+
+
+def main(args):
+  print("hello from hello")
+  db_url = os.environ.get("DATABASE_URL")
+  assert db_url is not None, "DATABASE_URL is missing!"
+  print("db_url found")
+  p = urlparse(db_url)
+  print("parsed")
+  conn = psycopg2.connect(
+      dbname="defaultdb",
+      user=p.username,
+      password=p.password,
+      port=p.port,
+      host=p.hostname,
+      sslmode="require",
+  )
+  print("connection created")
+  conn.close()
+  print("connection closed")
+  return {"body": str(44)}
